e2b03f980a171e4c5111f410d7e02124555dcb5a
Vikas Kumar Singh <visingh@nisum.com>
2020-03-27 01:16:03 +0530

    added the build

---
+++ build/precache-manifest.c3877d234e89c2bf90538a7cad90b6d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a25a181871734701dc9c78b3039859e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb3e13ed383c6f79d34",
+    "url": "/static/css/2.b00a0654.chunk.css"
+  },
+  {
+    "revision": "456747a35740d3fa34ae",
+    "url": "/static/css/main.df8a9326.chunk.css"
+  },
+  {
+    "revision": "0cb3e13ed383c6f79d34",
+    "url": "/static/js/2.937786de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.937786de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456747a35740d3fa34ae",
+    "url": "/static/js/main.b86490f1.chunk.js"
+  },
+  {
+    "revision": "d0d698e16c6db000b459",
+    "url": "/static/js/runtime-main.2309bdee.js"
+  }
+]);
